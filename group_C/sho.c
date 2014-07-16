8f106383b3a7076a2a24b7b1407c2b15735c725e
Anton Repko <anton@a-repko.sk>
2014-07-16 13:39:39 +0200

    added group_C folder for our group. Includes a code for spherical harmonic wavefunction (no dependencies). It is using recursion relations, so it is resistant to round-off errors.

---
+++ group_C/sho.c
@@ -0,0 +1,47 @@
+#include <math.h>
+
+// Anton Repko, 15th July 2014
+// spherical harmonic oscillator - calculation of R_nl(r) in
+// psi_nlm(r) = R_nl(r) * Y_lm(theta,phi)
+
+// utilized formula (either straight or through a special recursion):
+// R_nl(r) = 2 * (mw/pi)^0.25 * exp(-mw * r^2 / 2) * r^l
+// * sqrt(2^l * mw^(l+1) * (2n+2l+1)!! / (2^n * n!))
+// * sum(i=0..n){binom(i,n) * (-2*mw*r^2)^i / (2l+2i+1)!!}
+// where mw = m * omega / hbar
+
+// calculation through a recursion relation among functions
+// of constant (n+l), starting with n = 0
+double sho_wf (double r, double mw, int n, int l, int which)
+{
+  int i, npl;
+  double x, mwr2, cur, last, last2, factor;
+  if ((r < 0) || (mw < 0) || (n < 0) || (l < 0))
+    return 0.;
+  if (r == 0) {
+    if (l != 0)
+      return 0.;
+    factor = mw;
+    for (i = 1; i <= n; i++)
+      factor *= (i + 0.5) / i;
+    return 2 * sqrt(sqrt(mw / M_PI) * factor);
+  }
+  x = sqrt(mw) * r;
+  mwr2 = x * x;
+  npl = n + l;
+  factor = mw;
+  for (i = 3; i <= 2 * npl + 1; i += 2)
+    factor *= 2 * mwr2 / i;
+  last2 = sqrt(factor);
+  if (l == npl)
+    cur = last2;
+  last = ((npl + 0.5) / x - x) * last2;
+  if (l == npl - 1)
+    cur = last;
+  for (i = npl - 2; i >= l; i--) {
+    cur = (((i + 1.5) / x - x) * last - sqrt(npl - i - 1) * last2) / sqrt(npl - i);
+    last2 = last;
+    last = cur;
+  }
+  return 2 * pow(mw / M_PI, 0.25) * exp(-0.5*mwr2) * cur;
+}
---
+++ group_C/sho.h
@@ -0,0 +1,4 @@
+// spherical harmonic oscillator - calculation of R_nl(r) in
+// psi_nlm(r) = R_nl(r) * Y_lm(theta,phi)
+// where mw = m * omega / hbar
+double sho_wf(double r, double mw, int n, int l);
