05ee8edcbee2963f3f4c5c4f7829e3c9704d4643
Deni Vale <denivalesq@gmail.com>
2014-07-22 19:23:52 +0200

    changes

--- group_C/HF_L0/solver.h
+++ group_C/HF_L0/solver.h
@@ -1,3 +1,8 @@
+#ifndef _solver_h
+#define _solver_h
+#include "eigen.h"
+#include "potential.h"
+eig_t solve_HF(Vab_t **Vacbd, int N_dim, int N_occ);
 // solves the Hartree-Fock according to the matrix elements in Vacbd
 // return value will contain the resulting energies and eigenvectors
-eig_t solve_HF(Vab_t **Vacbd, int N_dim, int N_occ);
+#endif
