99a3f8232bccb86fd6bb29ceba57998801d61221
danielw <danielw@daniel.(none)>
2014-07-23 18:36:28 +0200

    Working HF for the l=0 subspace, difference from BM in first decimal. Code needs cleanup

--- KNW.for/project/src/numerical_integration.f90
+++ KNW.for/project/src/numerical_integration.f90
@@ -1,65 +1,76 @@
 MODULE numerical_integration
 
-USE types
+  USE types
 
-LOGICAL, protected :: is_init_grid_GH = .false.
-INTEGER, protected :: grid_size_GH
-REAL(KIND=r_kind), ALLOCATABLE, protected :: grid_points_GH(:)
-REAL(KIND=r_kind), ALLOCATABLE, protected :: grid_weights_GH(:), grid_weights_GL(:)
+  PRIVATE
 
-INTEGER, parameter :: factorial_table_max = 300
-REAL(KIND=r_kind), protected :: factorial_table(0:factorial_table_max)
+  PUBLIC :: is_init_grid_GH, grid_size_GH, grid_points_GH, grid_weights_GH, init_grid_GH, is_init_grid_GLag, grid_size_GLag, alpha_GLag, grid_points_GLag, grid_weights_GLag, init_grid_GLag
+
+  LOGICAL, protected :: is_init_grid_GH = .false.
+  INTEGER, protected :: grid_size_GH
+  REAL(KIND=r_kind), ALLOCATABLE, protected :: grid_points_GH(:)
+  REAL(KIND=r_kind), ALLOCATABLE, protected :: grid_weights_GH(:)
+
+  INTEGER, parameter :: factorial_table_max = 300
+  REAL(KIND=r_kind), protected :: factorial_table(0:factorial_table_max)
+
+  LOGICAL, protected :: is_init_grid_GLag = .false.
+  INTEGER, protected :: grid_size_GLag
+  REAL(KIND=r_kind), protected :: alpha_GLag
+  REAL(KIND=r_kind), ALLOCATABLE, protected :: grid_points_GLag(:)
+  REAL(KIND=r_kind), ALLOCATABLE, protected :: grid_weights_GLag(:)
+  
 
 
 CONTAINS
 
-! initializes grid points and wheigts for Gauss-Hermite integration, n is the number of grid points between r=0,infty. The order of the Hermite polynomial for the quadrature is 2*n
-SUBROUTINE init_grid_GH(n)
+  ! initializes grid points and wheigts for Gauss-Hermite integration, n is the number of grid points between r=0,infty. The order of the Hermite polynomial for the quadrature is 2*n
+  SUBROUTINE init_grid_GH(n)
 
-  IMPLICIT NONE
+    IMPLICIT NONE
 
-  INTEGER, intent(in) :: n
+    INTEGER, intent(in) :: n
 
-  REAL(kind=r_kind), parameter :: one_r = 1.0_r_kind
-  INTEGER :: ndfact, ndherm
-  REAL(kind=r_kind) :: eps
-  REAL(kind=r_kind), allocatable :: gpwrk(:),gwwrk(:)
+    REAL(kind=r_kind), parameter :: one_r = 1.0_r_kind
+    INTEGER :: ndfact, ndherm
+    REAL(kind=r_kind) :: eps
+    REAL(kind=r_kind), allocatable :: gpwrk(:),gwwrk(:)
 
-  INTEGER :: II
+    INTEGER :: II
 
-  grid_size_GH = n
+    grid_size_GH = n
 
-  IF(allocated(grid_points_GH)) DEALLOCATE(grid_points_GH)
-  IF(allocated(grid_weights_GH)) DEALLOCATE(grid_weights_GH)
-  ALLOCATE(grid_points_GH(grid_size_GH),grid_weights_GH(grid_size_GH))
-  
-  
+    IF(allocated(grid_points_GH)) DEALLOCATE(grid_points_GH)
+    IF(allocated(grid_weights_GH)) DEALLOCATE(grid_weights_GH)
+    ALLOCATE(grid_points_GH(grid_size_GH),grid_weights_GH(grid_size_GH))
 
-  CALL init_factorial_table
 
-  eps = 1e-14_r_kind
-  ndfact = factorial_table_max
-  ndherm = ndfact
-  
-  IF(2*grid_size_GH-1 > ndherm) THEN
+
+    CALL init_factorial_table
+
+    eps = 1e-14_r_kind
+    ndfact = factorial_table_max
+    ndherm = ndfact
+
+    IF(2*grid_size_GH-1 > ndherm) THEN
        WRITE(*,*) 'ERROR in subrotuine numerical_integration_ra, in module numerical_integration:'
        WRITE(*,*) 'grid_size_gh > ',(ndherm+1)/2,' is not supported, stopping.'
        STOP
     END IF
 
 
-  IF(allocated(gpwrk)) DEALLOCATE(gpwrk)
-  IF(allocated(gwwrk)) DEALLOCATE(gwwrk)
-  ALLOCATE(gpwrk(ndherm),gwwrk(ndherm))
-  gpwrk = 0.0_r_kind
-  gwwrk = 0.0_r_kind
+    IF(allocated(gpwrk)) DEALLOCATE(gpwrk)
+    IF(allocated(gwwrk)) DEALLOCATE(gwwrk)
+    ALLOCATE(gpwrk(ndherm),gwwrk(ndherm))
+    gpwrk = 0.0_r_kind
+    gwwrk = 0.0_r_kind
 
-  CALL HERMIT(2*grid_size_GH, gpwrk, gwwrk, eps, factorial_table, ndherm, ndfact)
-  
-  grid_points_GH = gpwrk(1:grid_size_GH)
-  grid_weights_GH = gwwrk(1:grid_size_GH)  
+    CALL HERMIT(2*grid_size_GH, gpwrk, gwwrk, eps, factorial_table, ndherm, ndfact)
 
-  DEALLOCATE(gpwrk,gwwrk)
+    grid_points_GH = gpwrk(1:grid_size_GH)
+    grid_weights_GH = gwwrk(1:grid_size_GH)  
+
+    DEALLOCATE(gpwrk,gwwrk)
 
 !!$  !for test, order 2,4 OK
 !!$  WRITE(*,*) 'order GH = ', 2*grid_size_GH
@@ -69,23 +80,23 @@ SUBROUTINE init_grid_GH(n)
 !!$  END DO
 !!$  !end for test
 
-  is_init_grid_GH = .true.
+    is_init_grid_GH = .true.
 
-END SUBROUTINE init_grid_GH
+  END SUBROUTINE init_grid_GH
 
-SUBROUTINE init_factorial_table
-  IMPLICIT NONE
-  
-  INTEGER :: II
-  
-  factorial_table(0) = 1.0_r_kind
+  SUBROUTINE init_factorial_table
+    IMPLICIT NONE
 
-  DO II=1,factorial_table_max
-     factorial_table(II) = II*factorial_table(II-1)
-  END DO
+    INTEGER :: II
+
+    factorial_table(0) = 1.0_r_kind
+
+    DO II=1,factorial_table_max
+       factorial_table(II) = II*factorial_table(II-1)
+    END DO
 
 
-END SUBROUTINE init_factorial_table
+  END SUBROUTINE init_factorial_table
 
 
 
@@ -120,9 +131,9 @@ END SUBROUTINE init_factorial_table
        write(*,*) 'module grid: ND/HERMI :',' NN-1 = ',NN-1,' NDFACT = ',NDFACT
        STOP
     end IF
-    
- !   IF (NDHERM.GT.NDFACT) stop 'module grid: ND/HERMI'
- !   IF (NN-1.GT.NDFACT) stop 'module grid: ND/HERMI'
+
+    !   IF (NDHERM.GT.NDFACT) stop 'module grid: ND/HERMI'
+    !   IF (NN-1.GT.NDFACT) stop 'module grid: ND/HERMI'
     !
     EEPS=EPS
     !
@@ -248,7 +259,7 @@ END SUBROUTINE init_factorial_table
     RETURN
   END SUBROUTINE H_ROOT
 
-    SUBROUTINE HRECUR(PN,DPN,PN1,X,NN)
+  SUBROUTINE HRECUR(PN,DPN,PN1,X,NN)
     USE types
     IMPLICIT REAL(KIND=r_kind) (A-H,O-Z)
     INTEGER NN
@@ -288,6 +299,123 @@ END SUBROUTINE init_factorial_table
     !
     RETURN
   END SUBROUTINE HRECUR
+
+
+  SUBROUTINE init_grid_GLag(n,alpha)
+    IMPLICIT NONE
+
+    INTEGER, intent(in) :: n
+    REAL(kind=r_kind), intent(in) :: alpha
+
+    INTEGER :: II 
+
+    IF(allocated(grid_points_GLag)) DEALLOCATE(grid_points_GLag)
+    IF(allocated(grid_weights_GLag)) DEALLOCATE(grid_weights_GLag)
+
+
+    grid_size_GLag = n
+    alpha_GLag = alpha
+    ALLOCATE(grid_points_GLag(n),grid_weights_GLag(n))
+
+    write(*,*) 'init_grid_GL: calling gauss_laguerre'
+    
+    CALL gauss_laguerre(grid_points_GLag,grid_weights_GLag,n,alpha)
+
+    write(*,*) 'init_grid_GL: done'
+
+!!$    !For debug
+!!$    WRITE(*,*) 'Generalized Gauss Laguerre'
+!!$    WRITE(*,*) 'grid_size_GLag = ',grid_size_GLag
+!!$    WRITE(*,*) 'alpha_GLag = ', alpha_GLag
+!!$    WRITE(*,'(2A16)') 'grid pts','grid weights'
+!!$    DO II = 1,grid_size_GLag
+!!$
+!!$       WRITE(*,'(2f16.10)') grid_points_GLag(II),grid_weights_GLag(II)
+!!$
+!!$    END DO
+    !end for debug
+    
+    
+
+    is_init_grid_GLag = .true.
+
+
+  END SUBROUTINE init_grid_GLag
+  
+
+
+
+  !  Sets up integration points for
+  !  Gaussian quadrature using Laguerre polynomials
+  SUBROUTINE gauss_laguerre(x,w,n,alf)
+    IMPLICIT NONE
+    INTEGER :: MAXIT
+    INTEGER, INTENT(IN) :: n
+    REAL(KIND=8) :: EPS
+    REAL(KIND=8), INTENT(IN) :: alf
+    REAL(KIND=8), INTENT(INOUT) :: w(n),x(n)
+    INTEGER :: i,its,j
+    REAL(KIND=8) :: ai,nn !ai,gammln, nn
+    REAL(KIND=8) :: p1,p2,p3,pp,z,z1
+
+    maxit = 10; eps = 3.E-14
+    nn = n
+    DO i=1,n
+       IF(i == 1)THEN
+          z=(1.+alf)*(3.+.92*alf)/(1.+2.4*n+1.8*alf)
+       ELSE IF(i == 2)THEN
+          z=z+(15.+6.25*alf)/(1.+9.0*alf+2.5*n)
+       ELSE
+          ai=i-2
+          z=z+((1.+2.55*ai)/(1.9*ai)+1.26*ai*alf/(1.+3.5*ai))* &
+               (z-x(i-2))/(1.+.3*alf)
+       ENDIF
+       DO its=1,MAXIT
+          p1=1.d0
+          p2=0.d0
+          DO  j=1,n
+             p3=p2
+             p2=p1
+             p1=((2*j-1+alf-z)*p2-(j-1+alf)*p3)/j
+          ENDDO
+          pp=(n*p1-(n+alf)*p2)/z
+          z1=z
+          z=z1-p1/pp
+          IF(ABS(z-z1) <= eps) GOTO 1
+       ENDDO
+       !PAUSE 'too many iterations in gaulag'
+       WRITE(*,*) 'too many iterations in gaulag'
+       STOP
+1      x(i)=z
+       w(i)=-EXP(gammln(alf+nn)-gammln(nn))/(pp*n*p2)
+       !write(*,*) x(i), w(i)
+    ENDDO
+
+  END SUBROUTINE gauss_laguerre
+
+ 
+ REAL(KIND =8) FUNCTION gammln(xx)
+    REAL(KIND=8), INTENT(IN) :: xx
+    INTEGER :: j
+    REAL(KIND=8) :: ser,tmp,x,y
+    REAL(KIND=8), DIMENSION(6) :: cof(6)
+    cof(1)=76.18009172947146; cof(2)=-86.50532032941677
+    cof(3)=24.01409824083091; cof(4)=-1.231739572450155
+    cof(5)= .1208650973866179E-2; cof(6) = -.5395239384953E-5
+    x=xx
+    y=x
+    tmp=x+5.5
+    tmp=(x+0.5)*LOG(tmp)-tmp
+    ser=1.000000000190015
+    DO j=1,6
+       y=y+1.0
+       ser=ser+cof(j)/y
+    ENDDO
+    gammln = tmp+LOG(2.5066282746310005*ser/x)
+
+  END FUNCTION gammln
+
+
 
 
 
