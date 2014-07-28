ef30672f76f6466b231f6623e76e116b42d0cb6d
Anton Repko <anton@a-repko.sk>
2014-07-28 18:31:46 +0200

    now using spherical HO with Laguerre from GSL (previous function errorneously gave zeros for small r and large n)

--- group_C/HF/sho.c
+++ group_C/HF/sho.c
@@ -1,17 +1,16 @@
 #include <math.h>
+#include <gsl/gsl_sf_laguerre.h>
 
-// Anton Repko, 16th July 2014
+// Anton Repko, 28th July 2014
 // spherical harmonic oscillator - calculation of R_nl(r) in
 // psi_nlm(r) = R_nl(r) * Y_lm(theta,phi)
 
-// corresponding formula (in fact, a special recursion relations are used instead):
+// corresponding formula (in fact, a function from GSL is used):
 // R_nl(r) = 2 * (mw/pi)^0.25 * exp(-mw * r^2 / 2) * r^l
 // * sqrt(2^l * mw^(l+1) * (2n+2l+1)!! / (2^n * n!))
 // * sum(i=0..n){binom(i,n) * (-2*mw*r^2)^i / (2l+2i+1)!!}
 // where mw = m * omega / hbar
 
-// calculation through a recursion relation among functions
-// of constant (n+l), starting with n = 0
 double sho_wf(double r, double mw, int n, int l)
 {
   int i, npl;
@@ -30,19 +29,10 @@ double sho_wf(double r, double mw, int n, int l)
   mwr2 = x * x;
   npl = n + l;
   factor = mw;
-  for (i = 3; i <= 2 * npl + 1; i += 2)
+  for (i = 1; i <= n; i++)
+    factor *= 2 * i / (double)(2 * i + 1);
+  for (i = 2 * n + 3; i <= 2 * npl + 1; i += 2)
     factor *= 2 * mwr2 / i;
-  cur = 0.;
-  last2 = sqrt(factor);
-  if (l == npl)
-    cur = last2;
-  last = ((npl + 0.5) / x - x) * last2;
-  if (l == npl - 1)
-    cur = last;
-  for (i = npl - 2; i >= l; i--) {
-    cur = (((i + 1.5) / x - x) * last - sqrt(npl - i - 1) * last2) / sqrt(npl - i);
-    last2 = last;
-    last = cur;
-  }
-  return 2 * pow(mw / M_PI, 0.25) * exp(-0.5*mwr2) * cur;
+  return 2 * pow(mw / M_PI, 0.25) * sqrt(factor) * exp(-0.5*mwr2)
+         * gsl_sf_laguerre_n(n, l + 0.5, mwr2);
 }
